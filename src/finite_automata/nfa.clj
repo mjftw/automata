8211db30a09a804a10e9308eb488f8832a105234
Merlin Webster <mjftwebster@gmail.com>
2021-02-20 21:28:54 +0000

    NFA can now process input or sequence of inputs

--- src/finite_automata/nfa.clj
+++ src/finite_automata/nfa.clj
@@ -1,11 +1,24 @@
-(ns finite-automata.dfa
+(ns finite-automata.nfa
   "Non-deterministic Finite Automata"
   (:require [clojure.set :as s]
-            [finite-automata.dfa-rules :as r]))
+            [finite-automata.utils :as u]
+            [finite-automata.nfa-rules :as r]))
 
 (defrecord NFA [current-states accept-states rules])
 
 (defn accepting? [nfa]
-  (let [current-states (set (:current-states nfs))
-        accept-states (set (:accept-states nfs))]
+  (let [current-states (set (:current-states nfa))
+        accept-states (set (:accept-states nfa))]
     (not (empty? (s/intersection current-states accept-states)))))
+
+(defn input [nfa value]
+  "Input a value or sequence of values into the NFA to get a new NFA"
+  (let [values (u/conform-seq value) {:keys [rules accept-states]} nfa]
+    (reduce
+     (fn [current-nfa next-value]
+       (->NFA
+        (r/next-states rules (:current-states current-nfa) next-value)
+        accept-states
+        rules))
+     nfa
+     values)))
--- test/finite_automata/nfa_test.clj
+++ test/finite_automata/nfa_test.clj
@@ -1,10 +1,27 @@
-(ns finite-automata.dfa-test
+(ns finite-automata.nfa-test
   (:require [clojure.test :refer :all]
             [finite-automata.nfa :refer :all]
-            [finite-automata.dfa-rules :as dfar]))
+            [finite-automata.dfa-rules :as r]))
 
-(deftest accepting?
+(def nfa (->NFA [1] [4] [(r/->FARule 1 "a" 1)
+                         (r/->FARule 1 "b" 1)
+                         (r/->FARule 1 "b" 2)
+                         (r/->FARule 2 "a" 3)
+                         (r/->FARule 2 "b" 3)
+                         (r/->FARule 3 "a" 4)
+                         (r/->FARule 3 "b" 4)]))
+
+(deftest test-accepting?
   (testing "should return true when NFA in an accept state"
     (is (true? (accepting? (->NFA [1 2] [2 3] [])))))
   (testing "should return false when NFA not in an accept state"
-    (is (true? (accepting? (->NFA [1 2] [4] []))))))
+    (is (false? (accepting? (->NFA [1 2] [4] []))))))
+
+(deftest test-input
+  (let [{:keys [accept-states rules]} nfa]
+    (testing "returns NFA with current-state updated when given a single value"
+      (is (= (->NFA [1] accept-states rules) (input nfa "a"))))
+    (testing "returns NFA with current-state updated when given a sequence of values"
+      (is (= (->NFA [1 4] accept-states rules) (input nfa ["b" "a" "a"]))))
+    (testing "returns NFA with current-state updated when given a string"
+      (is (= (->NFA [1 4] accept-states rules) (input nfa "baa"))))))
