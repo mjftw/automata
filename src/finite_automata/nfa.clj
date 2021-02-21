dbd933b50bc89ac4f334a7c9d5a8b58a03549e39
Merlin Webster <mjftwebster@gmail.com>
2021-02-21 22:39:42 +0000

    Added functions to convert a DFA to an NFA

--- src/finite_automata/nfa.clj
+++ src/finite_automata/nfa.clj
@@ -2,7 +2,9 @@
   "Non-deterministic Finite Automata"
   (:require [clojure.set :as s]
             [finite-automata.utils :as u]
-            [finite-automata.nfa-rules :as r]))
+            [finite-automata.dfa :as dfa]
+            [finite-automata.nfa-rules :as nr]
+            [finite-automata.dfa-rules :as dr]))
 
 (defrecord NFA [current-states accept-states rules])
 
@@ -21,13 +23,39 @@
          (->NFA
           (->>
            next-value
-           (r/next-states rules (:current-states current-nfa))
-           (r/follow-free-moves rules))
+           (nr/next-states rules (:current-states current-nfa))
+           (nr/follow-free-moves rules))
           accept-states
           rules))
        nfa
        values)
-      (->NFA (r/follow-free-moves rules current-states) accept-states rules))))
+      (->NFA (nr/follow-free-moves rules current-states) accept-states rules))))
 
 (defn accepts-input? [nfa values]
   (accepting? (input nfa values)))
+
+
+;; The issue with the current simulation is that free moves are not followed,
+;;  resulting in a dfa that doesn't jump to free move states when the NDA equivalent does.
+;;  This means that the NFA occasionally is able to reach an extra state over the DFA
+;;  Trying to fix this. Other than this issue they are equivalent.
+
+
+(defn discover-states-and-rules [nfa]
+  "Convert a non-deterministic finite automata to its deterministic equivalent"
+  (let [possible-inputs (set (map :input (:rules nfa)))]
+    (loop [known-states #{(set (:current-states nfa))}]
+      (let [rules (apply s/union
+                         (map #(nr/rules-for (:rules nfa) % possible-inputs) known-states))
+            next-states (->> rules (map #(dr/follow %)) (map #(nr/follow-free-moves (:rules nfa) %)) set)]
+        (if (s/subset? next-states known-states)
+          [known-states (seq rules)]
+          (recur (s/union known-states next-states)))))))
+
+(defn to-dfa [nfa]
+  "Convert a non-deterministic finite automata to its deterministic equivalent"
+  (let [[states rules] (discover-states-and-rules nfa)]
+    (dfa/->DFA
+     (:current-states nfa)
+     (set (filter #(accepting? (->NFA % (:accept-states nfa) #{})) states))
+     rules)))
