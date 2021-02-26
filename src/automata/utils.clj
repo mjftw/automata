221e989f9c04529db44627d607cf14d650b17da6
Merlin Webster <mjftwebster@gmail.com>
2021-02-26 19:55:59 +0000

    Added cross-map utility function

--- src/automata/utils.clj
+++ src/automata/utils.clj
@@ -8,6 +8,18 @@
                      seq2))
               seq1)))
 
+(defn cross-map [f coll]
+  "Find all pairwise combinations of a collection, and apply f to each pair.
+  Pairs will not include comparing an element to itself"
+  (for [a-and-idx (map list coll (range))
+        b-and-idx (map list coll (range))
+        :let [a (first a-and-idx)
+              b (first b-and-idx)
+              a-idx (second a-and-idx)
+              b-idx (second b-and-idx)]
+        :when (not= a-idx b-idx)]
+    (f a b)))
+
 (defn conform-seq [value]
   "Convert value to lazy sequence. String goes to sequence of char strings"
   (cond
