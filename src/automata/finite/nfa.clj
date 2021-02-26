b933f3db62e77e8268d7aa446207c9f2432f36bb
Merlin Webster <mjftwebster@gmail.com>
2021-02-26 23:57:42 +0000

    Remove unfinished nfa->dfa code

--- src/automata/finite/nfa.clj
+++ src/automata/finite/nfa.clj
@@ -33,29 +33,3 @@
 
 (defn accepts-input? [nfa values]
   (accepting? (input nfa values)))
-
-
-;; The issue with the current simulation is that free moves are not followed,
-;;  resulting in a dfa that doesn't jump to free move states when the NDA equivalent does.
-;;  This means that the NFA occasionally is able to reach an extra state over the DFA
-;;  Trying to fix this. Other than this issue they are equivalent.
-
-
-(defn discover-states-and-rules [nfa]
-  "Convert a non-deterministic finite automata to its deterministic equivalent"
-  (let [possible-inputs (set (map :input (:rules nfa)))]
-    (loop [known-states #{(set (:current-states nfa))}]
-      (let [rules (apply s/union
-                         (map #(nr/rules-for (:rules nfa) % possible-inputs) known-states))
-            next-states (->> rules (map #(dr/follow %)) (map #(nr/follow-free-moves (:rules nfa) %)) set)]
-        (if (s/subset? next-states known-states)
-          [known-states (seq rules)]
-          (recur (s/union known-states next-states)))))))
-
-(defn to-dfa [nfa]
-  "Convert a non-deterministic finite automata to its deterministic equivalent"
-  (let [[states rules] (discover-states-and-rules nfa)]
-    (dfa/->DFA
-     (:current-states nfa)
-     (set (filter #(accepting? (->NFA % (:accept-states nfa) #{})) states))
-     rules)))
